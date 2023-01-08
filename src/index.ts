c449a0b8cc807436a76e0f1284cbc1b06dfe569f
Huzaifa-Khalid-2 <Huzaifa-Khalid-2@users.noreply.github.com>
2023-01-08 14:00:56 +0000

    Prettified Code!

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,15 @@
 
 import promptSync from 'prompt-sync'
 
-function binarySearch(userArray: number[],
-    userNumber: number,
-    lowIndex: number, highIndex: number): number {
+function binarySearch(
+  userArray: number[],
+  userNumber: number,
+  lowIndex: number,
+  highIndex: number
+): number {
   const mid = (lowIndex + highIndex) / 2
   if (userNumber === userArray[mid]) {
-      return mid
+    return mid
   } else if (userNumber > userArray[mid]) {
     return binarySearch(userArray, userNumber, mid + 1, highIndex)
   } else {
@@ -22,38 +25,46 @@ function binarySearch(userArray: number[],
   }
 }
 
-    function main() {
-        console.log("Binary Search Program.")
-        try {
-            let randomNumberArray: number[] = new Array(250)
-            for (let counter = 0; counter < randomNumberArray.length; counter++) {
-                let randomNum: number
-                do {
-                    randomNum = Math.floor(Math.random() * 999)
-                } while (randomNumberArray.includes(randomNum)) {
-                    randomNumberArray[counter] = randomNum
-                }
-            }
-            let numberArray: number[] = randomNumberArray
-            numberArray.sort(function(a,b){return a - b})
-
-            console.log('\nSorted list of numbers:\n')
-            console.log(numberArray)
-            console.log('\n\n')
-
-            const prompt = promptSync()
-            const userInput = prompt('What number are you searching for in the array: ')
-            const userNumber = parseInt(userInput)
-            if (userNumber > 999 || userNumber < 0) {
-                throw new Error()
-            } else {
-                const searchResult = binarySearch(numberArray, userNumber, 0, numberArray.length)
-                console.log('')
-                console.log('Your number is in index: %d ', searchResult)
-            }
-        } catch (error) {
-          console.log('')
-          console.log('EROOR: Invalid Input')
-        }
+function main() {
+  console.log('Binary Search Program.')
+  try {
+    let randomNumberArray: number[] = new Array(250)
+    for (let counter = 0; counter < randomNumberArray.length; counter++) {
+      let randomNum: number
+      do {
+        randomNum = Math.floor(Math.random() * 999)
+      } while (randomNumberArray.includes(randomNum))
+      {
+        randomNumberArray[counter] = randomNum
+      }
     }
+    let numberArray: number[] = randomNumberArray
+    numberArray.sort(function (a, b) {
+      return a - b
+    })
+
+    console.log('\nSorted list of numbers:\n')
+    console.log(numberArray)
+    console.log('\n\n')
+
+    const prompt = promptSync()
+    const userInput = prompt('What number are you searching for in the array: ')
+    const userNumber = parseInt(userInput)
+    if (userNumber > 999 || userNumber < 0) {
+      throw new Error()
+    } else {
+      const searchResult = binarySearch(
+        numberArray,
+        userNumber,
+        0,
+        numberArray.length
+      )
+      console.log('')
+      console.log('Your number is in index: %d ', searchResult)
+    }
+  } catch (error) {
+    console.log('')
+    console.log('EROOR: Invalid Input')
+  }
+}
 main()
